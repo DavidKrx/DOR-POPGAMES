5374100ddef9c3eb39d1bce7e6d10faf26ade152
2dawa <2dawa@informatica.edu>
2024-12-03 10:07:15 +0000

    Botonsito modo light-dark

--- js/script.js
+++ js/script.js
@@ -1,19 +1,45 @@
-dia=document.getElementById("diaNoc");
-con=document.getElementById("contraste");
+dia=document.getElementById("modoNoche");
 
 function diurnoNocturno(){
-    if (dia.value=="diurno"){
-        if (con.value=="alto"){
-            document.body.setAttribute("class","diurno-alto");}
-        else if (con.value=="bajo") document.body.setAttribute("class","diurno");
-       }
-    else if (dia.value=="nocturno"){
-        if (con.value=="alto"){
-            document.body.setAttribute("class","nocturno-alto");
+    if (dia.getAttribute("class")=="fa-solid fa-moon"){
+    
+            dia.setAttribute("class","fa-solid fa-sun");
+            document.body.setAttribute("class","nocturno");
         }
-        else if (con.value=="bajo") document.body.setAttribute("class","nocturno");
-    }   
+    
+    else if (dia.getAttribute("class")=="fa-solid fa-sun"){
+        dia.setAttribute("class","fa-solid fa-moon");
+        document.body.setAttribute("class","diurno");
+}
 }
 
 
+/*
+JS
+// On page load set the theme.
+(function() {
+  let onpageLoad = localStorage.getItem("theme") || "";
+  let element = document.body;
+  element.classList.add(onpageLoad);
+  document.getElementById("theme").textContent =
+    localStorage.getItem("theme") || "light";
+})();
 
+function themeToggle() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+
+  let theme = localStorage.getItem("theme");
+  if (theme && theme === "dark-mode") {
+    localStorage.setItem("theme", "");
+  } else {
+    localStorage.setItem("theme", "dark-mode");
+  }
+
+  document.getElementById("theme").textContent = localStorage.getItem("theme");
+}
+
+html
+<button type="button" onclick="themeToggle()">Theme Toggle</button>
+<div id="theme"></div>
+*/
